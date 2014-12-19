8acb81a6d33b7bf482b5b85cc084cc228e0b2b06
Jason Hutchens <jason@agworld.com.au>
2014-12-19 15:09:05 +0800

    refactor common controller base

--- app/controllers/api/chats_controller.rb
+++ app/controllers/api/chats_controller.rb
@@ -1,11 +1,5 @@
 module Api
-  class ChatsController < ApplicationController
-    protect_from_forgery :with => :null_session
-    respond_to :json
-
-    rescue_from StandardError, :with => :_server_error
-    rescue_from ActionController::ParameterMissing, :with => :_bad_request
-
+  class ChatsController < BaseController
     def create
       @chat = Chat.new(params.permit(:user))
       if @chat.save
@@ -13,16 +7,6 @@ module Api
       else
         render json: @chat.errors, :status => :unprocessable_entity
       end
-    end
-
-    private
-
-    def _server_error(e)
-      render json: ["dag nabbit"], :status => :internal_server_error
-    end
-
-    def _bad_request(e)
-      render json: [e.message], :status => :bad_request
     end
   end
 end
--- app/controllers/api/inputs_controller.rb
+++ app/controllers/api/inputs_controller.rb
@@ -1,12 +1,5 @@
 module Api
-  class InputsController < ApplicationController
-    protect_from_forgery :with => :null_session
-    respond_to :json
-
-    rescue_from StandardError, :with => :_server_error
-    rescue_from ActionController::ParameterMissing, :with => :_bad_request
-    rescue_from ActiveRecord::RecordNotFound, :with => :_not_found
-
+  class InputsController < BaseController
     def create
       chat = Chat.find_by!(params.permit(:key))
       if chat.expired?
@@ -19,20 +12,6 @@ module Api
         @input = chat.input(params.permit(:text)[:text])
         render :show, :status => :created
       end
-    end
-
-    private
-
-    def _server_error(e)
-      render json: ["dag nabbit"], :status => :internal_server_error
-    end
-
-    def _bad_request(e)
-      render json: [e.message], :status => :bad_request
-    end
-
-    def _not_found(e)
-      render json: [e.message], :status => :not_found
     end
   end
 end
