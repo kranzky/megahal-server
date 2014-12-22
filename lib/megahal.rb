683e089e2291ba74399507f528398b12dcaf5e1e
Jason Hutchens <jason@agworld.com.au>
2014-12-22 22:36:41 +0800

    load and save

--- lib/megahal.rb
+++ lib/megahal.rb
@@ -1,20 +1,43 @@
+require 'fileutils'
+
 class MegaHAL
   def self.instance
     @@_instance ||= MegaHAL.new
   end
 
-  def self.init
+  def self.init(dir = nil)
     Rails.logger.info "[MH] Started Init"
-    # TODO: load brain if present, clear if otherwise
+    @@_dir = dir || Dir.getwd
+    @@_dirty = false
+    @@_checkpoint = Time.now
+    source = File.join(@@_dir, "megahal.brn")
+    if File.exists?(source)
+      instance.load(source)
+      Rails.logger.info "[MH] Loaded Brain"
+    else
+      instance.clear
+      Rails.logger.info "[MH] Cleared Brain"
+    end
     Rails.logger.info "[MH] Stopped Init"
   end
   
   def self.process
     unless job = _get_job
-      # TODO: save brain
-      sleep(2)
+      if @@_dirty && (Time.now - @@_checkpoint > 1.hour)
+        @@_dirty = false
+        @@_checkpoint = Time.now
+        source = File.join(@@_dir, "megahal.brn")
+        if File.exists?(source)
+          dest = File.join(@@_dir, "megahal_#{@@_checkpoint.strftime("%Y%m%d%H%M%S")}.brn")
+          FileUtils.mv(source, dest, force: true)
+        end
+        instance.save(source)
+        Rails.logger.info "[MH] Saved Brain"
+      end
+      sleep(0.2)
       return
     end
+    @@_dirty = true
     job.chat.reply(instance.reply(job.utterance.try(:text)))
     Rails.logger.info "[MH] Processed Job"
   end
