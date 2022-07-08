2a4d32d1e183fd885acfc7bd267eb18b4064db58
Jason Hutchens <jasonhutchens@gmail.com>
2022-07-08 11:16:57 +0800

    functionally works

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -3,9 +3,14 @@ class ChatController < ApplicationController
   end
 
   def say
-    name = params[:name]
-    message = params[:message]
-    response = "Why you say '#{message}'?"
+    @name = params[:name]
+    @message = params[:message]
+    if @message.present?
+      @response = "Why you say '#{@message}'?"
+    else
+      @response = "Hello #{@name}!" 
+    end
+    @message = "..." if @message&.length == 0
     respond_to do |format|
       format.turbo_stream
     end
