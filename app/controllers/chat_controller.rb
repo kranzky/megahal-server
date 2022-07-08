5fb32ebe5f26f440134744cac1e02d43a7130232
Jason Hutchens <jasonhutchens@gmail.com>
2022-07-08 10:46:21 +0800

    simple hotwire request/response

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -3,5 +3,11 @@ class ChatController < ApplicationController
   end
 
   def say
+    name = params[:name]
+    message = params[:message]
+    response = "Why you say '#{message}'?"
+    respond_to do |format|
+      format.turbo_stream
+    end
   end
 end
