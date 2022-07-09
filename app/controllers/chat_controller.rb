45a0f50bdb7de5fb490ed81f307cb9714bb4f1ca
Jason Hutchens <jasonhutchens@gmail.com>
2022-07-09 16:26:59 +0800

    beginnings of daemon communication via redis

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -1,4 +1,6 @@
 class ChatController < ApplicationController
+  before_action :_set_redis, only: [:say]
+
   def index
   end
 
@@ -6,9 +8,31 @@ class ChatController < ApplicationController
     @start = params[:start] == 'true'
     @name = params[:name]
     @message = params[:message]
-    @response = @start ? "Hello #{@name}!" : "Why you say '#{@message}'?"
+    @response = _megahal
     respond_to do |format|
       format.turbo_stream
     end
+  end
+
+  private
+
+  def _megahal
+    epoch = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+    key = "megahal-message:#{SecureRandom.uuid}"
+    data = { user: @name, message: @message }
+    @redis.set(key, data.to_json)
+    key.gsub!("-message:", "-reply:")
+    while Process.clock_gettime(Process::CLOCK_MONOTONIC) - epoch < 10
+      reply = @redis.getdel(key)
+      return reply if reply.present?
+      sleep 0.1
+    end
+    key.gsub!("-reply:", "-message:")
+    @redis.del(key)
+    return "[TIMEOUT]"
+  end
+
+  def _set_redis
+    @redis = Redis.new(url: ENV.fetch("REDIS_URL") { "redis://localhost:6379/1" })
   end
 end
